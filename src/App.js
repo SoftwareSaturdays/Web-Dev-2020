168f6d159b1889c238806cd1ef8c1416bff91533
adhikary97 <adhikary@purdue.edu>
2020-02-08 11:05:01 -0500

    first project portion

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Grid, Paper } from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <Grid justify="center" container spacing={5}>
+      <Grid item md={12}>
+        <Grid container spacing={5}>
+          <Grid item md={12}>
+            <Paper elevation={6}>Search...</Paper>
+          </Grid>
+          <Grid item md={8}>
+            <Paper elevation={6} style={{ height: '80%' }}>
+              Video
+            </Paper>
+            <Paper elevation={6} style={{ padding: '15px' }}>
+              Description
+            </Paper>
+          </Grid>
+          <Grid
+            container
+            item
+            xs={4}
+            direction="column"
+            justify="space-between"
+            alignItems="center"
+          >
+            <Paper elevation={6} style={{ padding: '30px' }}>
+              <div>Image</div>
+            </Paper>
+            <Paper elevation={6} style={{ padding: '30px' }}>
+              <div>Image</div>
+            </Paper>
+            <Paper elevation={6} style={{ padding: '30px' }}>
+              <div>Image</div>
+            </Paper>
+            <Paper elevation={6} style={{ padding: '30px' }}>
+              <div>Image</div>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+    );
+  }
 }
 
 export default App;
